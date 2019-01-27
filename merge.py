4a6acd430084b25941df7c2d8dfe2070fa2dc743
Shanmukha Yenneti <45616048+Shanmukha-Y@users.noreply.github.com>
2019-01-27 12:16:50 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img1 = cv2.imread('C:/Users/Shanmukha Yenneti/Desktop/sp (2).jpg')
-img2 = cv2.imread('C:/Users/Shanmukha Yenneti/Desktop/sketchesshr.png')
+img1 = cv2.imread('Groundtruthhere')  ##replace with Original image here
+img2 = cv2.imread('Sketchlocationhere') ## replace with the sketch here
 vis = np.concatenate((img1, img2), axis=1)
-cv2.imwrite('out.jpg', vis)
+cv2.imwrite('out.jpg', vis) ##mention the directory where you want the concatenated image to be saved
