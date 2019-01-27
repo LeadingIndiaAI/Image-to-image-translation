dce050cec49d66331cfe4e2e5808036e40f87fef
Shanmukha Yenneti <45616048+Shanmukha-Y@users.noreply.github.com>
2019-01-27 11:55:24 +0530

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -1,15 +1,14 @@
 import imageio
-img="C:/Users/Shanmukha Yenneti/Desktop/rr.jpeg"
+img="imagelocation"  ###enter your file location here!!!
 start_img = imageio.imread(img)
 
-#Y= 0.299 * R + 0.587 * G + 0.114 * B
 import numpy as np
 def grayscale(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 gray_img = grayscale(start_img)
 inverted_img = 255-gray_img
 import scipy.ndimage
-blur_img = scipy.ndimage.filters.gaussian_filter(inverted_img,sigma=30)
+blur_img = scipy.ndimage.filters.gaussian_filter(inverted_img,sigma=30) ### if you want a darker sketch increase the value of sigma
 def dodge(front,back):
     result=front*255/(255-back) 
     result[result>255]=255
@@ -18,4 +17,4 @@ def dodge(front,back):
 final_img= dodge(blur_img,gray_img)
 import matplotlib.pyplot as plt
 plt.imshow(final_img, cmap="gray")
-plt.imsave('C:/Users/Shanmukha Yenneti/Desktop/finalr.jpg', final_img, cmap='gray', vmin=0, vmax=255)
+plt.imsave('outputlocation', final_img, cmap='gray', vmin=0, vmax=255) ###enter the directory in which u want the image to be saved
